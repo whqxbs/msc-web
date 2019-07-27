e8d7a4ff41334c7bc66a6ad0d38cfa75bcd684da
yrpang <yrpang@outlook.com>
2019-07-27 10:23:51 +0800

    稍微修改福利题

--- web/views.py
+++ web/views.py
@@ -80,11 +80,12 @@ def register(request):
  
 def logout(request):
     if not request.session.get('is_login', None):
-        # 如果本来就未登录，也就没有登出一说
         return redirect("/")
     request.session.flush()
     return redirect("/")
 
 
 def tests(request):
+    if not request.session.get('is_login', None):
+        return redirect("/login/")
     return render(request, 'tests.html')
