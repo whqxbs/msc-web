38a22f69a0246fe1203e0ba102ab1cc84aa09a61
yrpang <yrpang@outlook.com>
2019-07-27 13:53:25 +0800

    add simpleui

--- mscweb/settings.py
+++ mscweb/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'simpleui',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+# STATIC_ROOT = './static'
